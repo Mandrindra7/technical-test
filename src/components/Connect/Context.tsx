9c440bcaaa7c7523c4fbfff88d9800ff5a58e147
Mandrindra <herymandrindra@gmail.com>
2023-01-27 17:56:19 +0300

    connect wallet and disconnect

--- src/components/Connect/Context.tsx
+++ src/components/Connect/Context.tsx
@@ -3,6 +3,18 @@
 import { createContext, useContext, useCallback, useState, PropsWithChildren } from 'react';
 import { useRouter } from 'next/navigation';
 
+import { MetaMaskInpageProvider } from "@metamask/providers";
+import Web3 from 'web3'
+import { Cookie } from '@next/font/google';
+import { COOKIE_NAME_PRERENDER_BYPASS } from 'next/dist/server/api-utils';
+import { connected } from 'process';
+
+declare global {
+  interface Window{
+    ethereum?:MetaMaskInpageProvider
+  }
+}
+
 export interface WalletContextValue {
   connected: boolean;
   account?: string;
@@ -23,36 +35,38 @@ export const WalletProvider = ({ children }: PropsWithChildren<WalletProviderPro
   const [state, setState] = useState<Omit<WalletContextValue, 'connect' | 'disconnect'>>({
     connected: false,
   });
+ 
 
   const connect = useCallback(async () => {
     if (state.connected) {
       Promise.resolve();
       return;
     }
-
-    // @todo: implement this flow
-    new Promise<void>((resolve, reject) => {
-      let confirmed = window.confirm('This function is among the challenges. Do you want to take it?');
-
-      if (confirmed) {
-        resolve();
-        router.push('/challenges/08-connect-wallet');
-        // setState({ connected: true, account: 'the wallet address here' });
-      } else {
-        reject(new Error('User rejected the connection'));
-      }
-    });
+    //0x6818b6Fd1D8b4363EB62dA50c1677794898B2adD
+    if(window!.ethereum){
+      // Do something 
+    
+      const res = await window.ethereum.request({method:'eth_requestAccounts'}) as any 
+      res && setState({ connected: true, account: res[0]});
+      res && router.push('/challenges/08-connect-wallet');
+    
+    }else
+      throw new Error('User rejected the connection')
+    
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state.connected]);
 
   const disconnect = useCallback(async () => {
-    if (!state.connected) {
-      Promise.resolve();
-      return;
+    const { ethereum } = window
+    if(ethereum) {
+      //const web3 = new Web3(window?.ethereum)
+      //web3.eth?.currentProvider?.connection?.close()
+      //web3.eth.defaultAccount = null;
+      //web3.accounts.deleteProperty()
+      //console.log(web3)
     }
-
-    // @todo: implement this flow
-    setState({ connected: false });
+   localStorage.clear()
+   setState({ connected: false , account: ''});
   }, [state.connected]);
 
   return (
