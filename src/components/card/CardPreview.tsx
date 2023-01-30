7de9394bea5a3b3f798714c63aaf8530919e9e54
Mandrindra <herymandrindra@gmail.com>
2023-01-30 21:14:39 +0300

    create card preview

--- src/components/card/CardPreview.tsx
+++ src/components/card/CardPreview.tsx
@@ -17,7 +17,71 @@ import './styles.css';
 function CardPreview() {
   return (
     <div className="preview-card">
-      <img alt="fake card" src="/images/card.png" />
+      <div className="card-head">
+          <div className='icon'>
+            <img src="/icons/back.svg" alt="back" className='back' />
+            <div className="market"> 
+              <img src="/icons/market.svg" alt="market" className='market-icon'/>
+              <div className="point"></div>
+            </div>
+            
+          </div>
+          <div className='content'>
+            <div style={{ marginBlock : 15}}>
+              <p className='title'>PLATFORM</p>
+              <span className='text'>PS5</span>
+            </div>
+            <div  style={{ marginBlock : 15}}>
+                 
+              <p className='title'>RELEASE</p>
+              <span className='text'>Fall 2020</span>
+            </div>
+           
+           <div  style={{ marginBlock : 15}}>
+              <p className='title'>PRICE</p>
+              <span className='text'>$50</span>
+           </div>
+           
+          </div>
+          <img src="/images/ps5.png" alt='ps5' className='ps5' />
+      </div>
+      <div className="card-body">
+        <div className="description">
+          <h1 className='d-title'>Dual Sense</h1>
+          <p className='d-text'>
+            DualSense also adds a build-in microphone array, 
+            which will enable players to easily chat
+            with friends without a headest... 
+          </p>
+        </div>
+        <div className="options">
+          <div className='item'>
+            <div className='option'></div>
+            <div style={{ display: 'flex', flexDirection: 'column'}}>
+              <p className='o-title'>Futuristic</p>
+              <span className='o-text'>Design</span>
+            </div>
+          </div>
+          <div className='item'>
+            <div className='option'></div>
+            <div style={{ display: 'flex', flexDirection: 'column'}}>
+              <p className='o-title'>Buitl-in</p>
+              <span className='o-text'>Microphone</span>
+            </div>
+          </div>
+        </div>
+        <div className="price">
+          <p className='p-text'>0.78 ETH</p>
+          <div className='button'>
+            <p className='b-text'>Buy</p>
+            <div className='line'></div>
+            
+            <img src='/icons/back.svg' alt='back' className='b-back'/>
+            
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
